28f68377edeca96df8e54e6cde06e2aeeaccabb3
Picred <andry4you@hotmail.it>
2023-11-05 20:07:08 +0100

    feat: itinere thread

---
+++ src/main/java/edu/unict/tswd/thread/ProvePassate/ValoreAssoluto/Shared.java
@@ -0,0 +1,28 @@
+package edu.unict.tswd.thread.ProvePassate.ValoreAssoluto;
+
+import java.util.Random;
+
+public class Shared {
+    Random rand = new Random();
+    private int x;
+
+    public Shared(){
+        this.x = rand.nextInt(11);
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public synchronized void setX(int t){
+        this.x = t;
+    }
+
+    public synchronized void sharedWait() throws InterruptedException{
+        wait();
+    }
+
+    public synchronized void sharedNotify(){
+        notifyAll();
+    }
+}
